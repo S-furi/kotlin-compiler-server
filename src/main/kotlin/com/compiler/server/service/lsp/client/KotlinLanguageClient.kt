48dc66982c399d3aaf75b61cbd7116fba8038ea3
Stefano Furi <stefano.furi@jetbrains.com>
2025-09-16 09:40:45 +0200

    chore: revert lsp client log level to debug

--- src/main/kotlin/com/compiler/server/service/lsp/client/KotlinLanguageClient.kt
+++ src/main/kotlin/com/compiler/server/service/lsp/client/KotlinLanguageClient.kt
@@ -12,26 +12,26 @@ class KotlinLanguageClient : LanguageClient {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun telemetryEvent(o: Any) {
-        logger.info(o.toString())
+        logger.debug(o.toString())
     }
 
     override fun publishDiagnostics(diagnostics: PublishDiagnosticsParams) {
-        diagnostics.diagnostics.forEach { d -> logger.info("Diagnostic: {} at {}", d.message, d.range) }
+        diagnostics.diagnostics.forEach { d -> logger.debug("Diagnostic: {} at {}", d.message, d.range) }
     }
 
     override fun showMessage(messageParams: MessageParams) {
-        logger.info("[{}]: {}", messageParams.type, messageParams.message)
+        logger.debug("[{}]: {}", messageParams.type, messageParams.message)
     }
 
     override fun showMessageRequest(params: ShowMessageRequestParams): CompletableFuture<MessageActionItem?>? {
-        logger.info("Message request: ${params.message}")
+        logger.debug("Message request: ${params.message}")
         params.actions?.forEach { action ->
-            logger.info("Action: ${action.title}")
+            logger.debug("Action: ${action.title}")
         }
         return CompletableFuture.completedFuture(params.actions?.firstOrNull())
     }
 
     override fun logMessage(message: MessageParams) {
-        logger.info("${message.message}")
+        logger.debug("${message.message}")
     }
 }
