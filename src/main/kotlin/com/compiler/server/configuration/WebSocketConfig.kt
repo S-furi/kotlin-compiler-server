ec16370021bc81fb4a82e144319669530cd04cfc
Stefano Furi <stefano.furi@jetbrains.com>
2025-09-22 09:44:53 +0200

    fix: increase websocket buffer size of 1MB

--- src/main/kotlin/com/compiler/server/configuration/WebSocketConfig.kt
+++ src/main/kotlin/com/compiler/server/configuration/WebSocketConfig.kt
@@ -2,10 +2,12 @@ package com.compiler.server.configuration
 
 import com.compiler.server.controllers.LspCompletionWebSocketHandler
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean
 
 @Configuration
 @EnableWebSocket
@@ -17,5 +19,14 @@ class WebSocketConfig : WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry.addHandler(handler, "/lsp/complete")
             .setAllowedOrigins(ACCESS_CONTROL_ALLOW_ORIGIN_VALUE)
+    }
+
+    @Bean
+    fun createWebSocketContainer(): ServletServerContainerFactoryBean {
+        return ServletServerContainerFactoryBean().apply {
+            maxTextMessageBufferSize = (1_000_000) // 1 MB
+            maxBinaryMessageBufferSize = (1_000_000)
+            maxSessionIdleTimeout = 0L
+        }
     }
 }
