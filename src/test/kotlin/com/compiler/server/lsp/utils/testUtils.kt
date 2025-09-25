8001a035508237bfbee8f2c4ce1a0d2e9dc8f3fd
Stefano Furi <stefano.furi@jetbrains.com>
2025-09-25 14:25:36 +0200

    refactor: use DSL-like syntax for caret extraction

--- src/test/kotlin/com/compiler/server/lsp/utils/testUtils.kt
+++ src/test/kotlin/com/compiler/server/lsp/utils/testUtils.kt
@@ -4,24 +4,24 @@ import org.eclipse.lsp4j.Position
 
 const val CARET_MARKER = "<CARET>"
 
-fun extractCaret(code: String): Pair<String, Position> {
-    val lines = code.lines().toMutableList()
+fun extractCaret(caretMarker: String = CARET_MARKER, code: () -> String): Pair<String, Position> {
+    val lines = code().lines().toMutableList()
     var caretLine = -1
     var caretChar = -1
 
     for ((i, line) in lines.withIndex()) {
-        val idx = line.indexOf(CARET_MARKER)
+        val idx = line.indexOf(caretMarker)
         if (idx != -1) {
             caretLine = i
             caretChar = idx
             // Remove the placeholder
-            lines[i] = line.removeRange(idx, idx + CARET_MARKER.length)
+            lines[i] = line.removeRange(idx, idx + caretMarker.length)
             break
         }
     }
 
     if (caretLine == -1 || caretChar == -1) {
-        throw IllegalArgumentException("No \"$CARET_MARKER\" marker found in code")
+        throw IllegalArgumentException("No \"$caretMarker\" marker found in code")
     }
 
     return lines.joinToString("\n") to Position(caretLine, caretChar)
